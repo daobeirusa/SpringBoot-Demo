9efbb4afae413494c70bcb3270034aef1cb8041e
Yom <vonscream@outlook.com>
2018-11-30 10:04:36 +0800

    自定义异常类返回json类型异常数据

--- src/main/java/com/example/demo/Controller/TestErrorController.java
+++ src/main/java/com/example/demo/Controller/TestErrorController.java
@@ -1,12 +1,18 @@
 package com.example.demo.Controller;
 
+import com.example.demo.bean.MyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class NumberFormatErrorController {
+public class TestErrorController {
     @RequestMapping("NumberFormatException")
     public String numberFormat()throws Exception{
         throw new NumberFormatException("数值转换异常");
+    }
+
+    @RequestMapping("jsonError")
+    public String jsonException()throws MyException{
+        throw new MyException("IO异常");
     }
 }
--- src/main/java/com/example/demo/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/Exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.demo.Exception;
 
+import com.example.demo.bean.ErrorInfo;
+import com.example.demo.bean.MyException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,5 +21,16 @@ public class GlobalExceptionHandler {
         mav.addObject("url",req.getRequestURL());
         mav.setViewName(DEFAULT_ERROR_VIEW);
         return mav;
+    }
+
+    @ExceptionHandler(value = MyException.class)
+    @ResponseBody
+    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req,MyException e)throws MyException{
+        ErrorInfo<String> r = new ErrorInfo<>();
+        r.setUrl(req.getRequestURL().toString());
+        r.setData("Json Data");
+        r.setMessage(e.getMessage());
+        r.setCode(ErrorInfo.ERROR);
+        return r;
     }
 }
